6a7e8d3eb7c5a3d0efe0d8e0f5b608d7b055b7a5
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-11 20:24:19 +0530

    Update Adder.py

--- Adder.py
+++ Adder.py
@@ -20,9 +20,9 @@ import traceback
 import time
 import random
 
-api_id = 0000000   #Enter Your 7 Digit Telegram API ID.
-api_hash = '56c2e6dbec19ebb226c728c0c1afe2bd'   #Enter Yor 32 Character API Hash
-phone = '+10000000000'   #Enter Your Mobilr Number With Country Code.
+api_id = 1709788   #Enter Your 7 Digit Telegram API ID.
+api_hash = '41a4fc8af8e7d911d4924ac307b25239'   #Enter Yor 32 Character API Hash
+phone = '+14246727855'   #Enter Your Mobilr Number With Country Code.
 client = TelegramClient(phone, api_id, api_hash)
 async def main():
     # Now you can use all client methods listed below, like for example...
