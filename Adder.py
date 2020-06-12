c4c21215f3a4a74991cc23c8f542d6e59eb41152
nmk10000 <58908219+nmk10000@users.noreply.github.com>
2020-06-12 18:21:53 +0530

    Update Adder.py

--- Adder.py
+++ Adder.py
@@ -1,4 +1,3 @@
-
 from telethon.sync import TelegramClient
 from telethon.tl.functions.messages import GetDialogsRequest
 from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
